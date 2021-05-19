16d1fa60db7bfa76dcd1ebd004f16e26e32191d6
RorckWebDev <nesterenkov.denis@gmail.com>
2021-05-19 16:40:01 +1000

    Header Slider-done

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,9 @@
-$('.single-item').slick();
+$(function (){
+    $('.header-slider__item').slick({
+        dots:true,
+        arrows:false,
+        fade: true,
+        autoplay: true
+    });
+    
+});
