29db3288048cd473679a8bbbd15d1d7da278f677
RorckWebDev <nesterenkov.denis@gmail.com>
2021-05-28 15:37:12 +1000

    done sadebar

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,24 @@
 $(function (){
+    $(".sidebar-price__form-input").ionRangeSlider({
+        type: "double",
+        onStart: function (data) 
+        {
+            $('.sidebar-price__form-from').text(data.from);
+            $('.sidebar-price__form-to').text(data.to);
+        },
+        onChange: function (data)
+        {
+            $('.sidebar-price__form-from').text(data.from);
+            $('.sidebar-price__form-to').text(data.to);
+        }
+        
+    });
+    $(".sidebar-recent__info-rating").rateYo({
+        readOnly: true,
+        starWidth: "11px",
+        normalFill: "#d6d6d6",
+        ratedFill: "#ffcc00",
+      });
     $('.header-slider__item').slick({
         dots:true,
         arrows:false,
